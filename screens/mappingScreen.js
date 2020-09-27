c1f15be1a1f751411ff6205e1c28d41339bc867f
Abhishek Sharma <sharmaash876@gmail.com>
2020-09-27 11:57:11 +0530

    Screens and Navigation added

---
+++ screens/mappingScreen.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const MappingScreen = () => {
+    return (
+        <View>
+            <Text>
+                Mapping Screen
+            </Text>
+        </View>
+    )
+}
+
+export default MappingScreen;
