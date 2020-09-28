36ea2c1b1e43580fc3d8bdf008675e9e99e2fcb8
Abhishek Sharma <sharmaash876@gmail.com>
2020-09-29 02:39:01 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,19 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import AppNavigator from "./navigation/appNavigator";
+
+import PlacesNavigator from './navigation/PlacesNavigator';
 
 export default function App() {
-  return (
-    <AppNavigator />
-  );
+  return <PlacesNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
